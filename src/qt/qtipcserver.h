96d48d8506499e1f5ebbfc571c3a5233486a3a3b
HardRockMusicCoin <hardrockmusiccoin@mail.com>
2017-10-17 17:42:54 -0400

    HardRockMusicCoin
    Hard Rock and Roll for Serious Fans!

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define HardRockMusicCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "HardRockMusicCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
